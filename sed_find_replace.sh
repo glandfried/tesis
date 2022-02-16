957a1412b66dbbd149de13e828a30a7b7d93cf65
Gustavo Landfried <gustavolandfried@gmail.com>
2022-02-16 19:14:00 -0300

    Tesis Landfried: Actualizo índice, y agrego anotaciones en intro desordenadas.

--- sed_find_replace.sh
+++ sed_find_replace.sh
@@ -3,4 +3,5 @@ sed -i 's/\\proglang{/\\texttt{/g' doc.tex
 sed -i 's/\\pkg{/\\texttt{/g' doc.tex
 sed -i 's/\\citep{/\\cite{/g' doc.tex
 sed -i 's/Imagenes\//figures\//g' doc.tex
-sed -i 's/\\section\*/\\section/g' doc.tex 
+sed -i 's/\\section\*/\\section/g' doc.tex
+sed -i 's/\\subsection\*/\\subsection/g' doc.tex 
